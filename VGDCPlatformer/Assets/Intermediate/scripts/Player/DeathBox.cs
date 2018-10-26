c0adb92aac3dd6a75317619f4b2bfa87539dc30e
Tony <toscarreks@gmail.com>
2018-10-26 16:50:32 -0700

    Added a Deathbox to scene, added "Player" tag to Player(Beginner)

--- VGDCPlatformer/Assets/Intermediate/scripts/Player/DeathBox.cs
+++ VGDCPlatformer/Assets/Intermediate/scripts/Player/DeathBox.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// In order to use this, you'll need to attach this and a BoxCollider2D Component
+// 
+
 public class DeathBox : MonoBehaviour {
 	public void OnCollisionEnter2D(Collision2D collision)
     {
-		Debug.Log("DEATH BOX HAS BEEN ENTERED");
         // If the player lands in the death box, kill the player. To death
         if (collision.gameObject.CompareTag("Player"))
         { 
-			Debug.Log("Twas the player");
 			// Kill the player by setting health to 0
+            Debug.Log("You were killed. To death.")
             collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(100000);
         }
     }
