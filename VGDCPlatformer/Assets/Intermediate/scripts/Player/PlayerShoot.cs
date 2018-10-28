6ef57469d6da9ee074db178b1b3fcf853a2654e2
davidp37 <djparra@uci.edu>
2018-10-28 15:49:49 -0700

    Shooting without visuals

--- VGDCPlatformer/Assets/Intermediate/scripts/Player/PlayerShoot.cs
+++ VGDCPlatformer/Assets/Intermediate/scripts/Player/PlayerShoot.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+
+    public GameObject shot;
+    public Rigidbody2D shotBody;
+
+
+
 	void Start () {
-		
+
+        int direction = -1;
+
+        Instantiate(shot);
+        GameObject Player = GameObject.Find("Player");
+        CharacterController2D controller = Player.GetComponent<CharacterController2D>();
+        if (controller.m_FacingRight)
+            direction = 1;
+        shotBody = GetComponent<Rigidbody2D>();
+        shotBody.velocity = new Vector2(50 * direction, 0);
+
+
 	}
 	
 	// Update is called once per frame
