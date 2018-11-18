af1b155c92c301fd5401416f6f3babe9afe5fd9b
davidp37 <djparra@uci.edu>
2018-11-18 15:17:44 -0800

    Door Scriptable object added

---
+++ VGDCPlatformer/Assets/Intermediate/scripts/mechanics/Door.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : ScriptableObject {
+
+	public GameObject Switch = GameObject.find.("Switch");
+	public Boolean open = false;
+	public Color selfColor = Color.Brown;
+	public string Name = "Door";
+}
---
+++ VGDCPlatformer/Assets/Intermediate/scripts/mechanics/Switch.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : ScriptableObject {
+
+	public GameObject Door = GameObject.find.("Door");
+	public Boolean pressed = false;
+	public Color selfColor = Color.Red;
+	public string Name = "Switch";
+}
