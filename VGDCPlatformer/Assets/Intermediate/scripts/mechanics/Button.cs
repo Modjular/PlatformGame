121c59881763972c486ed73de24a18786ffde63e
davidp37 <djparra@uci.edu>
2018-11-18 15:44:09 -0800

    Added OnEnable functions to the door and button

--- VGDCPlatformer/Assets/Intermediate/scripts/mechanics/Button.cs
+++ VGDCPlatformer/Assets/Intermediate/scripts/mechanics/Button.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Switch : ScriptableObject {
 
-	public GameObject Door = GameObject.find.("Door");
+	public string doorName = "Door";
+	public GameObject Door;
 	public Boolean pressed = false;
 	public Color selfColor = Color.Red;
 	public string Name = "Switch";
+	public int number = 1;
+
+	// Called when a new instance is created
+	public void OnEnable
+	{
+
+		doorName += number;
+		Door = GameObject.find.(doorName);
+		number++;
+
+	}
 }
--- VGDCPlatformer/Assets/Intermediate/scripts/mechanics/Door.cs
+++ VGDCPlatformer/Assets/Intermediate/scripts/mechanics/Door.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Door : ScriptableObject {
 
-	public GameObject Switch = GameObject.find.("Switch");
+	public string buttonName = "Button"
+	public GameObject Button;
 	public Boolean open = false;
 	public Color selfColor = Color.Grey;
 	public string Name = "Door";
+	public int number = 1;
 
 
+	//Called when a new instance is created
 	void OnEnable
 	{
+
 		private Rigidbody2D barrier;
 		barrier.bodyType = RigidbodyType2D.Static;
+
+		buttonName += number;
+		Button = GameObject.find.(buttonName);
+		number++;
+
 	}
 }
