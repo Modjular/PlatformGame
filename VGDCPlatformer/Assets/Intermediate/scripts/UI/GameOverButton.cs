10c15d84bd872787ad4c2074862bfc2c943fc2d0
Michael Luu <luuduson1@yahoo.com>
2018-11-28 23:53:29 -0800

    game over load last scene (wip), other house-keeping stuff i think

---
+++ VGDCPlatformer/Assets/Intermediate/scripts/UI/GameOverButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverButton : MonoBehaviour 
+{
+
+	public void RestartGame()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene();
+	}
+
+}
