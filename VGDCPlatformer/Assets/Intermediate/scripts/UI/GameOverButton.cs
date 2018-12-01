61a0f2b18ad70a8f4c8969f9ab9cd8c3442f25e3
Tony <toscarreks@gmail.com>
2018-12-01 15:52:08 -0800

    Fixed the GameOverButton bug

--- VGDCPlatformer/Assets/Intermediate/scripts/UI/GameOverButton.cs
+++ VGDCPlatformer/Assets/Intermediate/scripts/UI/GameOverButton.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverButton : MonoBehaviour 
 {
 
 	public void RestartGame()
 	{
-		UnityEngine.SceneManagement.SceneManager.LoadScene();
-	}
+        SceneManager.LoadScene("AlexScene");
+    }
 
 }
